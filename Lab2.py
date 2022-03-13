306a8332a3f75f667b35c38e975d08626b111004
= <dcnation1000000@gmail.com>
2022-03-13 12:16:12 -0400

    Finish lab 2

--- Lab2.py
+++ Lab2.py
@@ -5,14 +5,14 @@ class Queries():
 	def q1(self):
 		return """
 		CREATE TABLE singha_employees(
-			id INTEGER PRIMARY KEY,
+			id SERIAL PRIMARY KEY,
 			first_name VARCHAR(20),
 			last_name VARCHAR(20),
 			email VARCHAR(40),
 			age INTEGER
 		);
 		CREATE TABLE singha_salaries(
-			employee_id INTEGER REFERENCES singha_employees(id),
+			employee_id SERIAL REFERENCES singha_employees(id),
 			salary REAL
 		);
 	"""
@@ -20,55 +20,55 @@ class Queries():
 	def q2(self):
 		return """
 		INSERT INTO singha_employees 
-		(id, first_name, last_name, email, age) 
+		(first_name, last_name, email, age) 
 		VALUES
-			(0, 'anik', 'singha', 'anik.singha68@myhunter.cuny.edu', 18),
-			(1, 'Jack', 'Singer', 'jacksinger@gmail.com', 27),
-			(2, 'Nick', 'Smith', 'nicksmith@yahoo.com', 19),
-			(3, 'Tina', 'Smith', 'tinasmith@gmail.com', 24),
-			(4, 'Jona', 'Davis', 'jonadavis@outlook.com', 36),
-			(5, 'Joe', 'Jonas', 'joejonas@yahoo.com', 29),
-			(6, 'Dace', 'Goggins', 'dacegoggins@yahoo.com', 43),
-			(7, 'Alvin', 'Lam', 'alvinlam@gmail.com', 24),
-			(8, 'Novin', 'Tang', 'novintang@gmail.com', 18),
-			(9, 'No', 'Clue', 'noclue@yahoo.com', 44),
-			(10, 'Jake', 'Krants', 'jakekrantz@gmail.com', 26),
-			(11, 'Jimmy', 'Donalds', 'jimmydonalds@hotmail.com', 22),
-			(12, 'Chris', 'Pine', 'chrispine@gmail.com', 51),
-			(13, 'Pratt', 'Stevens', 'prattstevens@yahoo.com', 37),
-			(14, 'Steven', 'Universe', 'stevenuniverse@gmail.com', 19),
-			(15, 'Chris', 'Evans', 'chrisevans@outlook.com', 38),
-			(16, 'Robert', 'Downey', 'robertdowney@gmail.com', 53),
-			(17, 'Mark', 'Ruffalo', 'markruffalo@gmail.com', 54),
-			(18, 'Tom', 'Holland', 'tomholland@yahoo.com', 25),
-			(19, 'Steven', 'Strange', 'stevenstrange@hotmail.com', 35),
-			(20, 'Bennedict', 'Cumberbatch', 'bennedictcumberbatch@gmail.com', 38);
+			('anik', 'singha', 'anik.singha68@myhunter.cuny.edu', 18),
+			('Jack', 'Singer', 'jacksinger@gmail.com', 27),
+			('Nick', 'Smith', 'nicksmith@yahoo.com', 19),
+			('Tina', 'Smith', 'tinasmith@gmail.com', 24),
+			('Jona', 'Davis', 'jonadavis@outlook.com', 36),
+			('Joe', 'Jonas', 'joejonas@yahoo.com', 29),
+			('Dace', 'Goggins', 'dacegoggins@yahoo.com', 43),
+			('Alvin', 'Lam', 'alvinlam@gmail.com', 24),
+			('Novin', 'Tang', 'novintang@gmail.com', 18),
+			('No', 'Clue', 'noclue@yahoo.com', 44),
+			('Jake', 'Krants', 'jakekrantz@gmail.com', 26),
+			('Jimmy', 'Donalds', 'jimmydonalds@hotmail.com', 22),
+			('Chris', 'Pine', 'chrispine@gmail.com', 51),
+			('Pratt', 'Stevens', 'prattstevens@yahoo.com', 37),
+			('Steven', 'Universe', 'stevenuniverse@gmail.com', 19),
+			('Chris', 'Evans', 'chrisevans@outlook.com', 38),
+			('Robert', 'Downey', 'robertdowney@gmail.com', 53),
+			('Mark', 'Ruffalo', 'markruffalo@gmail.com', 54),
+			('Tom', 'Holland', 'tomholland@yahoo.com', 25),
+			('Steven', 'Strange', 'stevenstrange@hotmail.com', 35),
+			('Bennedict', 'Cumberbatch', 'bennedictcumberbatch@gmail.com', 38);
 
 
 		INSERT INTO singha_salaries 
-		(employee_id, salary) 
+		(salary) 
 		VALUES
-			(0, 154500),
-			(1, 54000),
-			(2, 67500),
-			(3, 84500),
-			(4, 47000),
-			(5, 93200),
-			(6, 71300),
-			(7, 44000),
-			(8, 56000),
-			(9, 63500),
-			(10, 46800),
-			(11, 340000),
-			(12, 125000),
-			(13, 84000),
-			(14, 54500),
-			(15, 37000),
-			(16, 225000),
-			(17, 78000),
-			(18, 110000),
-			(19, 93500),
-			(20, 48000);
+			(154500),
+			(54000),
+			(67500),
+			(84500),
+			(47000),
+			(93200),
+			(71300),
+			(44000),
+			(56000),
+			(63500),
+			(46800),
+			(340000),
+			(125000),
+			(84000),
+			(54500),
+			(37000),
+			(225000),
+			(78000),
+			(110000),
+			(93500),
+			(48000);
 	"""
 
 	def q3(self):
@@ -79,27 +79,24 @@ class Queries():
 
 	def q4(self):
 		return """
-		SELECT *
+		SELECT first_name, last_name
 		FROM singha_employees
-		WHERE id IS NOT NULL AND
-		first_name IS NOT NULL AND
-		last_name IS NOT NULL AND
-		email IS NOT NULL AND
-		AGE IS NOT NULL;
+		WHERE age = 18;
 	"""
 
 	def q5(self):
 		return """
 		SELECT salary + 8 AS new_salaries 
 		FROM singha_salaries
-		
 	"""
 
 	def q6(self):
 		return """
-		SELECT DISTINCT employee_id
-		FROM singha_employees;
-		
+		SELECT id FROM singha_employees
+		WHERE (first_name IS NULL 
+		AND email = 'anik.singha68@myhunter.cuny.edu') OR
+		(last_name IS NULL
+		AND email = 'anik.singha68@myhunter.cuny.edu');
 	"""
 
 	def q7(self):
--- Lab3.py
+++ Lab3.py
@@ -5,7 +5,7 @@ class Queries():
 	def q1(self):
 		return """
 		CREATE TABLE singha_customers_accounts(
-			id INTEGER PRIMARY KEY,
+			id SERIAL PRIMARY KEY,
 			first_name VARCHAR(20),
 			last_name VARCHAR(20),
 			bank_account_number VARCHAR(30),
@@ -15,121 +15,80 @@ class Queries():
 		);
 
 		CREATE TABLE singha_stocks(
-			id INTEGER PRIMARY KEY,
+			id SERIAL PRIMARY KEY,
 			symbol CHAR(3),
 			price REAL,
 			last_updated_at DATE
 		);
 
 		INSERT INTO singha_customers_accounts 
-		(id, first_name, last_name, bank_account_number, paypal_email, venmo_email, created_at) 
+		(first_name, last_name, bank_account_number, paypal_email, venmo_email, created_at) 
 		VALUES
-			(0, 'anik', 'singha', '0000', 'anik.singha68@myhunter.cuny.edu', 'anik.singha68@myhunter.cuny.edu', '2021-10-05'),
-			(1, 'Jack', 'Singer', '0001', 'jacksinger@gmail.com', 'jacksinger@gmail.com', '2021-09-04'),
-			(2, 'Nick', 'Smith', '0002', 'nicksmith@yahoo.com', 'nicksmith@yahoo.com', '2021-04-11'),
-			(3, 'Tina', 'Smith', '0003', 'tinasmith@gmail.com', 'tinasmith@gmail.com', '2021-06-14'),
-			(4, 'Jona', 'Davis', '0004', 'jonadavis@outlook.com', 'jonadavis@outlook.com', '2021-01-01'),
-			(5, 'Joe', 'Jonas', '0005', 'joejonas@yahoo.com', 'joejonas@yahoo.com', '2022-02-03'),
-			(6, 'Dace', 'Goggins', '0006', 'dacegoggins@yahoo.com', 'dacegoggins@yahoo.com', '2021-07-04'),
-			(7, 'Alvin', 'Lam', '0007', 'alvinlam@gmail.com', 'alvinlam@gmail.com', '2021-03-03'),
-			(8, 'Novin', 'Tang', '0008', 'novintang@gmail.com', 'novintang@gmail.com', '2021-07-08'),
-			(9, 'No', 'Clue', '0009', 'noclue@yahoo.com', 'noclue@yahoo.com', '2021-09-10');
+			('anik', 'singha', '0000', 'anik.singha68@myhunter.cuny.edu', 'anik.singha68@myhunter.cuny.edu', '2021-10-05'),
+			('Jack', 'Singer', '0001', 'jacksinger@gmail.com', 'jacksinger@gmail.com', '2021-09-04'),
+			('Nick', 'Smith', '0002', 'nicksmith@yahoo.com', 'nicksmith@yahoo.com', '2021-04-11'),
+			('Tina', 'Smith', '0003', 'tinasmith@gmail.com', 'tinasmith@gmail.com', '2021-06-14'),
+			('Jona', 'Davis', '0004', 'jonadavis@outlook.com', 'jonadavis@outlook.com', '2021-01-01'),
+			('Joe', 'Jonas', '0005', 'joejonas@yahoo.com', 'joejonas@yahoo.com', '2022-02-03'),
+			('Dace', 'Goggins', '0006', 'dacegoggins@yahoo.com', 'dacegoggins@yahoo.com', '2021-07-04'),
+			('Alvin', 'Lam', '0007', 'alvinlam@gmail.com', 'alvinlam@gmail.com', '2021-03-03'),
+			('Novin', 'Tang', '0008', 'novintang@gmail.com', 'novintang@gmail.com', '2021-07-08'),
+			('No', 'Clue', '0009', 'noclue@yahoo.com', 'noclue@yahoo.com', '2021-09-10');
 
 		INSERT INTO singha_stocks 
-		(id, symbol, price, last_updated_at)
+		(symbol, price, last_updated_at)
 		VALUES
-			(0, '$#@', 1000, '2021-01-14'),
-			(1, '$$#', 1450, '2021-07-03'),
-			(2, '@#', 1700, '2022-02-07'),
-			(3, '$@$', 950, '2021-09-10'),
-			(4, '$$$', 750, '2021-01-01');
+			('$#@', 1000, '2021-01-14'),
+			('$$#', 1450, '2021-07-03'),
+			('@#', 1700, '2022-02-07'),
+			('$@$', 950, '2021-09-10'),
+			('$$$', 750, '2021-01-01');
 	"""
 
 	def q2(self):
 		return """
-		INSERT INTO singha_employees 
-		(id, first_name, last_name, email, age) 
-		VALUES
-			(0, 'anik', 'singha', 'anik.singha68@myhunter.cuny.edu', 18),
-			(1, 'Jack', 'Singer', 'jacksinger@gmail.com', 27),
-			(2, 'Nick', 'Smith', 'nicksmith@yahoo.com', 19),
-			(3, 'Tina', 'Smith', 'tinasmith@gmail.com', 24),
-			(4, 'Jona', 'Davis', 'jonadavis@outlook.com', 36),
-			(5, 'Joe', 'Jonas', 'joejonas@yahoo.com', 29),
-			(6, 'Dace', 'Goggins', 'dacegoggins@yahoo.com', 43),
-			(7, 'Alvin', 'Lam', 'alvinlam@gmail.com', 24),
-			(8, 'Novin', 'Tang', 'novintang@gmail.com', 18),
-			(9, 'No', 'Clue', 'noclue@yahoo.com', 44),
-			(10, 'Jake', 'Krants', 'jakekrantz@gmail.com', 26),
-			(11, 'Jimmy', 'Donalds', 'jimmydonalds@hotmail.com', 22),
-			(12, 'Chris', 'Pine', 'chrispine@gmail.com', 51),
-			(13, 'Pratt', 'Stevens', 'prattstevens@yahoo.com', 37),
-			(14, 'Steven', 'Universe', 'stevenuniverse@gmail.com', 19),
-			(15, 'Chris', 'Evans', 'chrisevans@outlook.com', 38),
-			(16, 'Robert', 'Downey', 'robertdowney@gmail.com', 53),
-			(17, 'Mark', 'Ruffalo', 'markruffalo@gmail.com', 54),
-			(18, 'Tom', 'Holland', 'tomholland@yahoo.com', 25),
-			(19, 'Steven', 'Strange', 'stevenstrange@hotmail.com', 35),
-			(20, 'Bennedict', 'Cumberbatch', 'bennedictcumberbatch@gmail.com', 38);
-
-		INSERT INTO singha_salaries 
-		(employee_id, salary) 
-		VALUES
-			(0, 154500),
-			(1, 54000),
-			(2, 25000),
-			(3, 84500),
-			(4, 47000),
-			(5, 93200),
-			(6, 71300),
-			(7, 44000),
-			(8, 56000),
-			(9, 63500),
-			(10, 15500),
-			(11, 340000),
-			(12, 125000),
-			(13, 84000),
-			(14, 54500),
-			(15, 37000),
-			(16, 225000),
-			(17, 32000),
-			(18, 110000),
-			(19, 33000),
-			(20, 24000);
+		SELECT first_name, last_name
+		FROM singha_customers_accounts
+		WHERE created_at::TEXT ~'-[0,1][7-9]|0-';
 	"""
 
 	def q3(self):
 		return """
-		SELECT DISTINCT employee_id
-		FROM singha_salaries;
+		SELECT symbol
+		FROM singha_stocks
+		WHERE last_updated_at > '2016-01-01' AND last_updated_at < '2026-01-01';
 	"""
 
 	def q4(self):
 		return """
-		SELECT id
-		FROM singha_employees
-		WHERE id IS NOT NULL;
+		SELECT SUM(price) AS "total market price",
+		MAX(price) AS "highest priced stock",
+		MIN(price) AS "lowest price stock",
+		COUNT(last_updated_at)
+		From singha_stocks
+		WHERE last_updated_at = '2021-09-10';
 	"""
 
 	def q5(self):
 		return """
-		SELECT salary + 8 
-		AS new_salaries 
-		FROM singha_salaries;
+		SELECT symbol,
+		CASE
+			WHEN price >= 3557.0 THEN 'SELL'
+			WHEN price < 3557.0 THEN 'BUY'
+		END AS "Action"
+		FROM singha_stocks;
 	"""
 
 	def q6(self):
 		return """
-		SELECT *
-		FROM singha_employees
-		WHERE id IS NOT NULL;
-		
-	"""
-
-	def q7(self):
-		return """
-		SELECT employee_id
-		FROM singha_salaries
-		WHERE salary < 16307
-		LIMIT 13;
+		SELECT first_name, 
+		last_name,
+		CASE
+			When paypal_email IS NOT NULL then paypal_email
+			WHEN paypal_email IS NULL THEN bank_account_number
+			WHEN bank_account_number IS NULL THEN venmo_email
+			WHEN venmo_email IS NULL THEN NULL
+		END AS "Payment method"
+		FROM singha_customers_accounts
+		WHERE first_name != 'anik';
 	"""
