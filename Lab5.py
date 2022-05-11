ff80bff1caef40da029c3708951fd0395536846b
= <dcnation1000000@gmail.com>
2022-05-11 16:56:13 -0400

    Finish Lab 5 and start 6

--- Lab5.py
+++ Lab5.py
@@ -1,35 +1,118 @@
+
 import psycopg2
 
 class Queries():
-    """
-    Please submit this file on gradescope.
-    Questions will appear when you submit to gradescope, there is no pentalty for resubmitting on gradescope.
-    It is advised to copy paste the question you see when you submit on gradescope under each function name in case you need the question offline
-    Put your solutions on the line following the line that starts with 'return' for each question
-    """
+	
+	def q1(self):
+		return """
+		CREATE TABLE singha_accounts(
+			id SERIAL PRIMARY KEY, 
+			first_name VARCHAR(40), 
+			last_name VARCHAR(40), 
+			email VARCHAR(40)
+		);
 
-    def q1(self):
-	"""Paste question here."""
-	return """
+		CREATE TABLE singha_credit_cards(
+			id SERIAL PRIMARY KEY, 
+			account_id INTEGER REFERENCES singha_accounts(id), 
+			card_limit REAL
+		);
+
+		CREATE TABLE singha_credit_card_transactions(
+			id INTEGER, 
+			card_id INTEGER REFERENCES singha_credit_cards(id), 
+			amount REAL
+		);
+
+		INSERT INTO singha_accounts
+		(first_name, last_name, email)
+		VALUES
+			('anik', 'singha', 'anik.singha68@myhunter.cuny.edu'),
+			('Jack', 'Singer','jacksinger@gmail.com'),
+			('Nick', 'Smith', 'nicksmith@yahoo.com'),
+			('Tina', 'Smith', 'tinasmith@gmail.com'),
+			('Jona', 'Davis', 'jonadavis@outlook.com'),
+			('Joe', 'Jonas', 'joejonas@yahoo.com'),
+			('Dace', 'Goggins', 'dacegoggins@yahoo.com'),
+			('Alvin', 'Lam', 'alvinlam@gmail.com'),
+			('Novin', 'Tang', 'novintang@gmail.com'),
+			('No', 'Clue', 'noclue@yahoo.com');
+
+		INSERT INTO singha_credit_cards
+		(account_id, card_limit)
+		VALUES
+			(1,5000),
+			(2,5000),
+			(3,5000),
+			(4,5000),
+			(5,5000),
+			(6,5000),
+			(7,5000),
+			(8,5000),
+			(9,5000),
+			(1,5000);
+		
+		INSERT INTO singha_credit_card_transactions
+		(id, card_id, amount)
+		VALUES
+			(1,1,500),
+			(2,2,400),
+			(3,3,660),
+			(4,4,200),
+			(5,5,800),
+			(6,6,700),
+			(7,7,400),
+			(8,8,300),
+			(9,9,100),
+			(3,3,600),
+			(2,2,800),
+			(1,1,200),
+			(6,6,700),
+			(4,4,900),
+			(3,3,400),
+			(6,6,600),
+			(7,7,800),
+			(8,8,200),
+			(2,2,500),
+			(9,9,600),
+			(10,10,800);
 	"""
 
-    def q2(self):
-	"""Paste question here."""
-	return """
+	def q2(self):
+		return """
+		SELECT singha_accounts.last_name, singha_accounts.email
+		FROM singha_accounts
+		INNER JOIN singha_credit_cards
+		ON singha_accounts.id = singha_credit_cards.account_id
+		WHERE last_name != 'singha';
 	"""
 
-    def q3(self):
-	"""Paste question here."""
-	return """
-	"""
-    
-    def q4(self):
-	"""Paste question here."""
-	return """
+	def q3(self):
+		return """
+		SELECT card_id, COUNT(card_id)
+		FROM singha_credit_card_transactions
+		GROUP BY card_id;
 	"""
 
-    def q5(self):
-	"""Paste question here."""
-	return """
+	def q4(self):
+		return """
+		SELECT email, card_id, card_limit, amount
+		FROM singha_accounts
+		INNER JOIN singha_credit_cards
+		ON singha_accounts.id = singha_credit_cards.account_id 
+		INNER JOIN singha_credit_card_transactions
+		ON singha_credit_cards.id = singha_credit_card_transactions.card_id
+		WHERE last_name != 'singha';
 	"""
 
+	def q5(self):
+		return """
+		SELECT first_name, last_name, email
+		FROM singha_accounts
+		INNER JOIN singha_credit_cards
+		ON singha_accounts.id = singha_credit_cards.account_id 
+		WHERE singha_credit_cards.id NOT IN (
+			SELECT card_id
+			FROM singha_credit_card_transactions
+		);
+	"""
--- Lab6.py
+++ Lab6.py
@@ -1,39 +1,102 @@
 import psycopg2
 
 class Queries():
-    """
-    Please submit this file on gradescope.
-    Questions will appear when you submit to gradescope, there is no pentalty for resubmitting on gradescope.
-    It is advised to copy paste the question you see when you submit on gradescope under each function name in case you need the question offline
-    Put your solutions on the line following the line that starts with 'return' for each question
-    """
+	
+	def q1(self):
+		return """
+		CREATE TABLE singha_users(
+			id SERIAL PRIMARY KEY,
+			username VARCHAR(40),
+			password VARCHAR(40),
+			email VARCHAR(60)
+		);
 
-    def q1(self):
-	"""Paste question here."""
-	return """
+		CREATE TABLE singha_posts(
+			id SERIAL PRIMARY KEY,
+			user_id INTEGER REFERENCES singha_users(id),
+			content TEXT
+		);
+
+		INSERT INTO singha_users
+		(username, password, email)
+		VALUES
+			('aniksingha', 'singhaanik', 'anik.singha68@myhunter.cuny.edu'),
+			('Jack', 'Singer', 'jacksinger@gmail.com'),
+			('Nick', 'Smith', 'nicksmith@yahoo.com'),
+			('Tina', 'Smith', 'tinasmith@gmail.com'),
+			('Jona', 'Davis', 'jonadavis@outlook.com'),
+			('Joe', 'Jonas', 'joejonas@yahoo.com'),
+			('Dace', 'Goggins', 'dacegoggins@yahoo.com'),
+			('Alvin', 'Lam', 'alvinlam@gmail.com'),
+			('Novin', 'Tang', 'novintang@gmail.com'),
+			('No', 'Clue', 'noclue@yahoo.com'),
+			('Jake', 'Krants', 'jakekrantz@gmail.com'),
+			('Jimmy', 'Donalds', 'jimmydonalds@hotmail.com'),
+			('Chris', 'Pine', 'chrispine@gmail.com'),
+			('Pratt', 'Stevens', 'prattstevens@yahoo.com'),
+			('Steven', 'Universe', 'stevenuniverse@gmail.com'),
+			('Chris', 'Evans', 'chrisevans@outlook.com'),
+			('Robert', 'Downey', 'robertdowney@gmail.com'),
+			('Mark', 'Ruffalo', 'markruffalo@gmail.com'),
+			('Tom', 'Holland', 'tomholland@yahoo.com'),
+			('Steven', 'Strange', 'stevenstrange@hotmail.com'),
+			('Bennedict', 'Cumberbatch', 'bennedictcumberbatch@gmail.com');
+
+		INSERT INTO singha_posts
+		(user_id, content)
+		VALUES
+			(1, 'hello'),
+			(2, 'my'),
+			(3, 'friend'),
+			(4, 'how'),
+			(5, 'have'),
+			(6, 'you'),
+			(7, 'been'),
+			(8, 'these'),
+			(9, 'past'),
+			(10, 'couple'),
+			(11, 'years.'),
+			(12, 'It has been'),
+			(13, 'a long'),
+			(14, 'time'),
+			(15, 'since we'),
+			(16, 'last'),
+			(17, 'spoke'),
+			(18, 'I hope'),
+			(19, 'you have'),
+			(20, 'been well');
 	"""
 
-    def q2(self):
-	"""Paste question here."""
-	return """
+	def q2(self):
+		return """
+		SELECT username, AVG(LENGTH(content))
+		FROM singha_users
+		INNER JOIN singha_posts
+		ON singha_users.id = singha_posts.user_id
+		WHERE username != 'aniksingha'
+		GROUP BY username;
 	"""
 
-    def q3(self):
-	"""Paste question here."""
-	return """
-	"""
-    
-    def q4(self):
-	"""Paste question here."""
-	return """
+	def q3(self):
+		return """
+		
 	"""
 
-    def q5(self):
-	"""Paste question here."""
-	return """
+	def q4(self):
+		return """
+		SELECT username, email
+		FROM singha_users
+		WHERE LENGTH(username) = 10 AND
+		STRPOS(username,')
 	"""
 
-    def q6(self):
-	"""Paste question here."""
-	return """
+	def q5(self):
+		return """
+		
 	"""
+
+	def q6(self):
+		return """
+		
+	"""
+
